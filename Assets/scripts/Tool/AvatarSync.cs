930448dd165e123a0090f1566ea615e4a08d11e1
xiangwei71 <xiangwei71@gmail.com>
2017-04-21 17:32:07 +0800

    加上場景clone
    雪人拿著一把傘，傘的上面還有一個小雪人..

---
+++ Assets/scripts/Tool/AvatarSync.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvatarSync : MonoBehaviour {
+
+    public Transform stage;
+    public Transform player;
+    public Transform[] avatar;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        Vector3 localPos =stage.transform.InverseTransformPoint(player.position);
+
+        //Qworld = Qstage * Qlocal;
+        //(Qstage)^-1*Qworld= Qlocal;
+        Quaternion Qlocal = Quaternion.Inverse(stage.transform.rotation) * player.transform.rotation;
+
+        foreach (Transform t in avatar)
+        {
+            if (t != null)
+            {
+                t.localPosition = localPos;
+                t.localRotation = Qlocal;
+            }
+            
+        }
+
+    }
+}
