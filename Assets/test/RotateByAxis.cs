eccc862a0d8dfdb4680c24abc0ea95f3f00bbb28
xiangwei71 <xiangwei71@gmail.com>
2017-05-25 18:37:15 +0800

    測試：
    直接把原本的T、N旋轉對齊averageNormal，並不能保證在球上那些T是重疊的

---
+++ Assets/test/RotateByAxis.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateByAxis : MonoBehaviour {
+
+    public Transform target;
+	// Use this for initialization
+	void Start () {
+        Vector3 targetNormal = target.up;
+        Vector3 nowNormal = transform.up;
+        Vector3 axis =Vector3.Cross(nowNormal,targetNormal);
+        float dotValue =Vector3.Dot(nowNormal,targetNormal);
+        float degree =Mathf.Rad2Deg*Mathf.Acos(dotValue);
+        print(degree);
+
+        Quaternion rot = Quaternion.AngleAxis(degree,axis);
+        transform.rotation = rot * transform.rotation;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
