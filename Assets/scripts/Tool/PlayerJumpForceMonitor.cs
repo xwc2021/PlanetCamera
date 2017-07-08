849f2d0aec2b34b4887d4b5e010278075c7420cc
xiangwei71 <xiangwei71@gmail.com>
2017-07-08 16:21:51 +0800

    補上上一版移漏的更新

---
+++ Assets/scripts/Tool/PlayerJumpForceMonitor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJumpForceMonitor : MonoBehaviour,JumpForceMonitor {
+
+    public PlanetPlayerController planetPlayerController;
+    public float jumpForceScaleLow = 1000;
+    public float jumpForceScaleHight = 2000;
+    public float getJumpForceStrength()
+    {
+        //按住fire鈕才加速
+        if (planetPlayerController.holdFire())
+            return jumpForceScaleHight;
+        else
+            return jumpForceScaleLow;
+    }
+}
