d42f868a7e029b0b2d97fd2881037286b20e14f1
xiangwei71 <xiangwei71@gmail.com>
2017-07-08 21:26:00 +0800

    更新ball_super_finn.prefab

--- Assets/scripts/Tool/PlayerJumpForceMonitor.cs
+++ Assets/scripts/Tool/PlayerJumpForceMonitor.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class PlayerJumpForceMonitor : MonoBehaviour,JumpForceMonitor {
 
     public PlanetPlayerController planetPlayerController;
-    static float jumpForceScaleLow = 1200;
-    static float jumpForceScaleHight = 2000;
+    public float jumpForceScaleLow = 1200;
+    public float jumpForceScaleHight = 2000;
     public float getJumpForceStrength()
     {
         //按住fire鈕才加速
--- Assets/scripts/Tool/PlayerMoveForceMonitor.cs
+++ Assets/scripts/Tool/PlayerMoveForceMonitor.cs
@@ -6,10 +6,11 @@ public class PlayerMoveForceMonitor : MonoBehaviour, MoveForceMonitor
 {
     public PlanetPlayerController planetPlayerController;
 
-    static float moveForceScaleOnAir = 60;
+    public float moveForceScaleOnAirLow = 60;
+    public float moveForceScaleOnAirHight = 60;
 
-    static float moveForceScaleLow = 80;
-    static float moveForceScaleHight = 90;
+    public float moveForceScaleLow = 80;
+    public float moveForceScaleHight = 90;
     public float getMoveForceStrength(bool isOnAir)
     {
         if (!isOnAir)
@@ -23,7 +24,10 @@ public class PlayerMoveForceMonitor : MonoBehaviour, MoveForceMonitor
         else
         {
             //如果在空中時的移動加速度和在地面時一樣，會感覺在空中偏快
-            return moveForceScaleOnAir;
+            if (planetPlayerController.holdFire())
+                return moveForceScaleOnAirHight;
+            else
+                return moveForceScaleOnAirLow;
         }
         
     }
