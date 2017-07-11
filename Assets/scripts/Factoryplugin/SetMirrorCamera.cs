ddaa4db77c4b7a983010c1df5fa720c206271950
xiangwei71 <xiangwei71@gmail.com>
2017-07-11 21:04:22 +0800

    加上SetMirrorCamera用來綁定MirrorCamera
    不過multiplayerPlanet關卡是由網路元件產生player，所以還不知要如何處理。

---
+++ Assets/scripts/Factoryplugin/SetMirrorCamera.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetMirrorCamera : MonoBehaviour,FactoryPlugin {
+
+    public MirrorCamera mirrorCamera;
+    public void doIt(GameObject gameObject)
+    {
+        Camera camera =gameObject.GetComponentInChildren<Camera>();
+        Debug.Assert(camera != null);
+        mirrorCamera.refCamera = camera;
+    }
+}
