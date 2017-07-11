130f33f3281028328e9b4661e1465f5d25536109
xiangwei71 <xiangwei71@gmail.com>
2017-07-11 20:20:03 +0800

    移除掉多餘的layer
    仔細想想，只要有tag就能作出攻擊的判定了

--- Assets/scripts/Shared/LayerDefined.cs
+++ Assets/scripts/Shared/LayerDefined.cs
@@ -6,8 +6,6 @@ public class LayerDefined  {
 
     public static int Simulate = LayerMask.NameToLayer("Simulate");
     public static int GravitySensor = LayerMask.NameToLayer("GravitySensor");
-    public static int AttackBlock = LayerMask.NameToLayer("AttackBlock");
-    public static int HitArea = LayerMask.NameToLayer("HitArea");
     public static int ground = LayerMask.NameToLayer("ground");
     public static int Block = LayerMask.NameToLayer("Block");
     public static int canJump = LayerMask.NameToLayer("canJump");
