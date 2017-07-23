e4f06a9c9616fbf9dc89d66bcb346a53448fe62c
xiangwei71 <xiangwei71@gmail.com>
2017-07-23 18:20:40 +0800

    可以撞金幣

---
+++ Assets/scripts/Machine/QuestionBox.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestionBox : MonoBehaviour {
+
+    bool doOnce = false;
+    bool useTimer = false;
+    float nowTime = 0;
+    public float usefulTime = 0.5f;
+    Transform originalBoxTransform;
+    public Animator originalBoxAnimator;
+    public MeshRenderer originalBox;
+    public MeshRenderer nullBox;
+    int finalHash = Animator.StringToHash("Base Layer.final");
+    QBoxReady qBoxReady;
+    QBoxFinal qBoxFinal;
+    QBoxDoScale qBoxDoScale;
+
+    void initAnimator()
+    {
+        qBoxReady = originalBoxAnimator.GetBehaviour<QBoxReady>();
+        qBoxFinal = originalBoxAnimator.GetBehaviour<QBoxFinal>();
+        qBoxDoScale = originalBoxAnimator.GetBehaviour<QBoxDoScale>();
+        Debug.Assert(qBoxReady != null);
+        Debug.Assert(qBoxFinal != null);
+        Debug.Assert(qBoxDoScale != null);
+        qBoxReady.qBox = this;
+        qBoxFinal.qBox = this;
+        qBoxDoScale.qBox = this;
+    }
+
+    // Use this for initialization
+    void Start() {
+        originalBoxTransform = originalBox.transform;
+        initAnimator();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        originalBoxAnimator.SetBool("doScale", true);
+        useTimer = true;
+    }
+
+    private void Update()
+    {
+        if (useTimer)
+            nowTime = nowTime + Time.deltaTime;
+    }
+
+    public bool timeIsOver()
+    {
+        if (useTimer && doOnce)
+            return nowTime >= usefulTime;
+        else
+            return false;
+    }
+
+    public void doFinalAction()
+    {
+        originalBox.enabled = false;
+        nullBox.enabled = true;
+    }
+
+    public void hitBox()
+    {
+        doOnce = true;
+    }
+
+}
