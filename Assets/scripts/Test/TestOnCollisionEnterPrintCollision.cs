0b4dd70b55c8fc8bdb430f2ed630556172888cdf
xiangwei71 <xiangwei71@gmail.com>
2017-07-26 11:44:25 +0800

    測試onCollisionEnter
    在1個有Rigidbody的Cube底下加入1個childCube，childCube只有Collider沒有Rigidbody，為Cube和childCube加上TestOnCollisionEnterPrintSelf；在另一個有Rigidbody的anotherRigidCube加上TestOnCollisionEnterPrintCollision。
    
    結果發現：
    (1)無論是Cube還是childCube碰到其他coliider/rigid時都會印出Cube。
    (2)當anotherRigidCube碰到Cube時會印出collision = Cube collider = Cube；anotherRigidCube碰到childCube時會印出collision = Cube collider = childCube。

---
+++ Assets/scripts/Test/TestOnCollisionEnterPrintCollision.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestOnCollisionEnterPrintCollision : MonoBehaviour {
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        print("collision = "+collision.gameObject.name+" colider = "+collision.collider.name);
+    }
+}
