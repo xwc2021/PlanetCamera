d8209c7c8ac8f75a2217199a0e6adc1dc1fde12d
xiangwei71 <xiangwei71@gmail.com>
2017-07-27 19:07:49 +0800

    加上電覽車的開關
    發現一個奇怪的現象，AirCableCa和Cable貼的太近有可能發動不了(要發動好幾次才會開始移動)

---
+++ Assets/scripts/Machine/DisalbeAirCableMotor.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisalbeAirCableMotor : MonoBehaviour {
+
+    public HingeJoint hingeJoint;
+    private void OnTriggerEnter(Collider other)
+    {
+        hingeJoint.useMotor = false;
+    }
+}
---
+++ Assets/scripts/Machine/EnableAirCableCarMotor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableAirCableCarMotor : MonoBehaviour {
+
+    public HingeJoint hingeJoint;
+    Material material;
+    public float force = 1500;
+
+    private void Awake()
+    {
+        material = GetComponent<MeshRenderer>().material;
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        material.color = Color.yellow;
+        JointMotor motor = hingeJoint.motor;
+        motor.targetVelocity = force;
+        hingeJoint.motor = motor;
+        hingeJoint.useMotor = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        material.color = Color.gray;
+    }
+}
