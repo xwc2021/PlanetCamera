15f0bc6bfdac20d780a144ef1cb3c40678ebe7dd
xiangwei71 <xiangwei71@gmail.com>
2017-07-27 15:43:46 +0800

    加入空中覽車

---
+++ Assets/scripts/Machine/PosFollow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PosFollow : MonoBehaviour {
+
+    public Transform followTarget;
+    public float speed;
+    private void FixedUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, followTarget.position, speed * Time.fixedDeltaTime);
+    }
+}
