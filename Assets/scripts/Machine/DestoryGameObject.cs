814d2f385498a24aedaee652b4f7ef6a658ae842
xiangwei71 <xiangwei71@gmail.com>
2017-07-28 12:27:23 +0800

    加入流籠

---
+++ Assets/scripts/Machine/DestoryGameObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestoryGameObject : MonoBehaviour {
+
+    public GameObject lockstitch;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(lockstitch);
+    }
+}
