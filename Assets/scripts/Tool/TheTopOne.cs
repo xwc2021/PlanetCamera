a4333f6871a21bfcd18f5647c576a08fbe18a98d
xiangwei71 <xiangwei71@gmail.com>
2017-08-03 15:16:01 +0800

    把Canvas放到finn底下，會導致Mobile Single Stick Control晃動

---
+++ Assets/scripts/Tool/TheTopOne.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheTopOne : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        transform.parent = null;
+	}
+	
+	
+}
