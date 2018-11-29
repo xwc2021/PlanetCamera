e6f91c72da306bbe3816721e7ab7200364293d35
xiangwei71 <xiangwei71@gmail.com>
2018-11-29 23:41:19 +0800

    實作TorusGravityGenerator

---
+++ Assets/Editor/TorusGravityGeneratorGizmoDrawer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class TorusGravityGeneratorGizmoDrawer
+{
+    [DrawGizmo(GizmoType.Selected | GizmoType.Active)]
+    static void DrawGizmoForMyScript(TorusGravityGenerator scr, GizmoType gizmoType)
+    {
+        Vector3 position = scr.transform.position;
+        var R = scr.R;
+        var r = scr.r;
+        var axisX = scr.transform.right;
+        var axisY = scr.transform.up;
+        var axisZ = scr.transform.forward;
+
+        //大圓
+        drawCircle(position, R, axisX, axisY);
+
+        //小圓
+
+        drawCircle(position + axisX * R, r, axisX, axisZ);
+    }
+
+    static void drawCircle(Vector3 center, float R, Vector3 axisX, Vector3 axisY)
+    {
+        var n = 50;
+        var two_pi = 2.0f * Mathf.PI;
+        var diff = two_pi / n;
+        for (var i = 0; i < n; ++i)
+        {
+            var radian = i * diff;
+            var next_radian = (radian + diff) % two_pi;
+            var from = center + R * (axisX * Mathf.Cos(radian) + axisY * Mathf.Sin(radian));
+            var to = center + R * (axisX * Mathf.Cos(next_radian) + axisY * Mathf.Sin(next_radian));
+            Debug.DrawLine(from, to, Color.green);
+        }
+    }
+}
--- Assets/scripts/GravityGenerator/TorusGravityGenerator.cs
+++ Assets/scripts/GravityGenerator/TorusGravityGenerator.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class TorusGravityGenerator : MonoBehaviour, GroundGravityGenerator
 {
+    public float R = 10.0f;
+    public float r = 2.0f;
     public Vector3 findGroundUp(ref Vector3 targetPos)
     {
-        return Vector3.zero;
+        var v = transform.InverseTransformPoint(targetPos);
+        var radian = Mathf.Atan2(v.y, v.x);
+
+        var axisX = transform.right;
+        var axisY = transform.up;
+        var gravicty_p = transform.position + R * (axisX * Mathf.Cos(radian) + axisY * Mathf.Sin(radian));
+
+        var temp = Vector3.Normalize(targetPos - gravicty_p);
+        return temp;
     }
 }
