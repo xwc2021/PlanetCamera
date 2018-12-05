1f57674edd661514abb675e7c05e2bbf661f87ff
xiangwei71 <xiangwei71@gmail.com>
2018-12-05 21:37:59 +0800

    測試一下把平面上的頂點投影到球面的效果

---
+++ Assets/scripts/Tool/ShowMeshBound.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowMeshBound : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        var meshFilter = this.GetComponent<MeshFilter>();
+        var bounds = meshFilter.sharedMesh.bounds;
+        print(bounds.max);
+
+        Debug.DrawLine(
+            transform.TransformPoint(bounds.min),
+            transform.TransformPoint(bounds.max),
+             Color.yellow);
+    }
+}
