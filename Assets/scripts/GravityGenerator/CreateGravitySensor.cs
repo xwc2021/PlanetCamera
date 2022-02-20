92effeecfdc86b651a721ba087043756ff4e13cf
xwc2022 <xiangwei71@example.com>
2022-02-20 19:54:21 +0800

    修改CreateGrvitySensor的作法

--- Assets/scripts/GravityGenerator/CreateGravitySensor.cs
+++ Assets/scripts/GravityGenerator/CreateGravitySensor.cs
@@ -3,34 +3,22 @@ public class CreateGravitySensor : MonoBehaviour
 {
     public Transform gs;
 
-    //GravitySensor的作用：GravisySensor的axix forward會指出地面(平面)的方向
-    //使用時記得產生後再旋轉物件，不然位置會對不到
-    //要改成旋轉後可以使用的話，要再加一層GameObject，然後把動態產生的GS都放到它底下，之後旋轉GameObject就行了，以後再說
+    // GravitySensor的作用：GravisySensor的axix forward會指出地面(平面)的方向
+    // 使用時記得產生後再旋轉物件，不然位置會對不到
     public void createGS()
     {
         Mesh mesh = GetComponent<MeshFilter>().sharedMesh;
 
-        int triCount = mesh.triangles.Length / 3;
-        int[] triangles = mesh.triangles;
-
+        var modelPos = transform.position;
+        int vCount = mesh.vertices.Length;
         Vector3[] vertices = mesh.vertices;
-        for (int i = 0; i < triCount; i++)
+        Vector3[] normals = mesh.normals;
+        for (int i = 0; i < vCount; i++)
         {
-            int v0 = triangles[3 * i];
-            int v1 = triangles[3 * i + 1];
-            int v2 = triangles[3 * i + 2];
-
-            Vector3 vector01 = vertices[v1] - vertices[v0];
-            Vector3 vector02 = vertices[v2] - vertices[v0];
-
-            //方向錯的話，作外積時交換vector01和vector02就行了
-            //不過就結果來說，三角形應該是順時針order
-            Vector3 Normal = Vector3.Cross(vector01, vector02);
-            Quaternion rot = Quaternion.LookRotation(Normal);
-
-            Vector3 center = transform.position + (vertices[v0] + vertices[v1] + vertices[v2]) / 3;
-
-            Instantiate(gs, center, rot, this.transform);
+            var v0 = vertices[i];
+            var n0 = normals[i];
+            Quaternion rot0 = Quaternion.LookRotation(n0);
+            Instantiate(gs, v0 + modelPos, rot0, this.transform);
         }
     }
 }
